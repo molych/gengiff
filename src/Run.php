68c1fcb328a51848fd384008af85bef93d1aa70d
Oleksandr Mochalov <molych69@gmail.com>
2020-09-18 02:27:42 +0300

    add Docopt

---
+++ src/Run.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Gendiff\Run;
+
+use Docopt;
+
+const DOC = <<<DOC
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
+  
+Options:
+  -h --help                     Show this screen
+  -v --version                  Show version
+  --format <fmt>                Report format [default: stylish]
+DOC;
+
+function run() 
+{
+    $args = Docopt::handle(DOC);
+
+    return $args;
+}    
